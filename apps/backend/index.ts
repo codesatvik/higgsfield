be7ed2f754671ce0ee71abd48357cef39e291037
Satvik <codesatvik@gmail.com>
2026-08-15 23:52:43 +0530

    implement user signup and avatar creation with Prisma integration , zod implementation

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,10 +1,39 @@
 import express from "express";
 const app = express();
 app.use(express.json());
+import z from "zod"
+import { CreateAvatarSchema, CreateUserSchema } from "./types";
+import { prisma } from "./db";
 
-app.post("/signup", (req, res) => { 
 
+app.post("/signup", async (req, res) => { 
+    const { success, data } = CreateUserSchema.safeParse(req.body);
+    if (!success) { 
+        res.status(411).json({
+            message:"incorrect credentials"
+        })
+        return;
+    }
+    const user = await prisma.user.create({
+        data: {
+             username : req.body.username,
+             password : req.body.password
+        }
+    })
+    res.json({message: "signed up",id :user.id})
 })
 app.post("/signin", (req, res) => { 
     res.json({})
 })
+
+app.post("/avatar", (req, res) => { 
+    const { success, data } = CreateAvatarSchema.safeParse(req.body);
+    if (!success) { 
+        res.status(401).json({
+            message: "incorrect"
+        });
+        return
+    }
+})
+
+app.listen(3000)
---
+++ apps/backend/types.ts
@@ -0,0 +1,10 @@
+import { xid, z } from "zod";
+
+export const CreateUserSchema = z.object({
+    username: z.string(),
+    password: z.string()
+})
+export const CreateAvatarSchema = z.object({
+    name: z.string(),
+    images:z.array(z.string())
+})
